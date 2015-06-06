6c70bb0e90369c8006c69a253990b3de5c774aa0
Nina Roby <ninaroby.usa@gmail.com>
2015-06-05 17:57:09 -0700

    updates

---
+++ log_object.js
@@ -0,0 +1,19 @@
+
+//create objects with array of properties
+var house = {
+    bedroom: ['bed', 'dresser', 'wardrobe', 'chest']
+    livingroom: ['sofa', 'coffee table', 'armchair', 'television']
+    kitchen: ['table', 'chairs' 'cooker', 'fridge']
+}
+
+//create a function that prints out the object and arrays
+
+//var print(Object.getOwnPropertyNames(house).sort()); 
+
+function printObject() {
+
+    for (var i in object)
+        console.log(object[i]);
+}
+
+
